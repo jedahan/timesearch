223749f1f24ed5c81cef0bf549017fd66657c8b6
Jonathan Dahan <jonathan@jonathan.is>
2015-09-24 15:29:00 -0400

    We have twitter oauth working wow

--- backend.js
+++ backend.js
@@ -1,15 +1,68 @@
-const config = require('./config.json')
 var {keywords} = require('./keywords.json')
 
+// DATABASE
 const nedb = require('nedb');
 const jobs = new nedb({ filename: 'tweets.db', autoload: true });
 jobs.persistence.setAutocompactionInterval(10*1000);
 const fs = require('fs')
-const ipc = require('ipc')
 
 // POPUPS
-const open = require('open')
 const notifier = require('node-notifier')
+
+// AUTH
+const ipc = require('ipc')
+const twitterApi = require('node-twitter-api')
+const twitter = new twitterApi({
+    consumerKey: process.env.CONSUMER_KEY,
+    consumerSecret: process.env.CONSUMER_SECRET,
+    callback: 'http://127.0.0.1:5000/oauth'
+});
+
+var http = require('http');
+
+http.createServer(function (req, res) {
+  const query = require('url').parse(req.url, true).query
+  console.log(query)
+  twitter.getAccessToken(
+    process.env.REQUEST_TOKEN,
+    process.env.REQUEST_TOKEN_SECRET,
+    query.oauth_verifier, function(error, accessToken, accessTokenSecret, results) {
+      if (error) {
+        console.log(error);
+      } else {
+        process.env.ACCESS_TOKEN = accessToken;
+        process.env.ACCESS_TOKEN_SECRET = accessTokenSecret;
+        fs.writeFile('./config.json', JSON.stringify({
+            consumer_key: process.env.CONSUMER_KEY
+            , consumer_secret:  process.env.CONSUMER_SECRET
+            , access_token: process.env.ACCESS_TOKEN
+            , access_token_secret:  process.env.ACCESS_TOKEN_SECRET
+          }), function(err){
+            if (err) throw err;
+            exports.changeStream(keywords.join(", "))
+        })
+      }
+    });
+  res.writeHead(301, {"Location": 'index.html'});
+  res.end();
+}).listen(5000, "127.0.0.1");
+
+const util = require('util');
+exports.authenticate = function authenticate(callback) {
+  if(!process.env.REQUEST_TOKEN && !process.env.REQUEST_TOKEN_SECRET){
+    twitter.getRequestToken(function(error, requestToken, requestTokenSecret, results){
+        if (error) {
+            console.log("Error getting OAuth request token : " + util.inspect(error));
+        } else {
+            process.env.REQUEST_TOKEN = requestToken;
+            process.env.REQUEST_TOKEN_SECRET = requestTokenSecret;
+            callback(twitter.getAuthUrl(process.env.REQUEST_TOKEN))
+        }
+    });
+  } else {
+    callback(twitter.getAuthUrl(process.env.REQUEST_TOKEN))
+  }
+}
 
 //GETTING
 const Twit = require('Twit')
@@ -24,7 +77,12 @@ exports.changeStream = function changeStream(newKeywords) {
   fs.writeFile('./keywords.json', JSON.stringify({ keywords }), function(err){
     if (err) throw err;
     console.log(`we are now tracking ${keywords}`)
-    t = new Twit(config)
+    t = new Twit({
+      consumer_key: process.env.CONSUMER_KEY
+    , consumer_secret:  process.env.CONSUMER_SECRET
+    , access_token: process.env.ACCESS_TOKEN
+    , access_token_secret:  process.env.ACCESS_TOKEN_SECRET
+    })
     stream = t.stream('user', { track: keywords })
 
     stream.on('tweet', (tweet) => {
@@ -33,7 +91,7 @@ exports.changeStream = function changeStream(newKeywords) {
       }
     });
 
-    t.get('statuses/home_timeline', {track: keywords, count: 20}, function(err, data, response){
+    t.get('statuses/home_timeline', {track: keywords, count: 100}, function(err, data, response){
       if(!err) { data.forEach((tweet) => { processTweet(tweet) }) }
     });
 
@@ -66,8 +124,7 @@ const notify = function(tweet){
     title: `@${tweet.screen_name}`,
     message: tweet.text,
     open: tweet.url,
+    customPath: __dirname,
     wait: true
   })
 }
-
-exports.changeStream(keywords.join(", "))
--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 "use strict";
 var menubar = require('menubar')
-var mb = menubar({width: 200, height: 40})
+var menu = menubar({height: 800, preloadWindow: true})
 
 var ipc = require('ipc')
 
-mb.on('ready', function ready () {
-  require('electron-compile').init();
+const config = require('./config.json')
+process.env.CONSUMER_KEY = config.consumer_key
+process.env.CONSUMER_SECRET = config.consumer_secret
+process.env.ACCESS_TOKEN = config.access_token
+process.env.ACCESS_TOKEN_SECRET = config.access_token_secret
 
+menu.on('ready', function ready () {
+  require('electron-compile').init();
   var app = require('./backend.js')
 
   ipc.on('keywords', (_, keywords) => app.changeStream(keywords) )
 
+  if(process.env.ACCESS_TOKEN=="" || process.env.ACCESS_TOKEN_SECRET=="") {
+    app.authenticate(function(url) {
+      menu.window.loadUrl(url)
+      menu.showWindow()
+    })
+  }
   console.log("menubar ready")
 })
