4c374aa97fece5fcddcc17a7e8aecc61dac46968
Jonathan Dahan <jonathan@jonathan.is>
2015-09-25 15:45:56 -0400

    Inline app.js in index.html

--- app.js
+++
@@ -1,9 +0,0 @@
-const ipc = require('ipc')
-
-const keywordsInput = document.querySelector('#keywords')
-
-keywordsInput.addEventListener('change', function(evt) {
-  ipc.send('keywords', this.value)
-})
-
-keywordsInput.value = require('./keywords.json').keywords.join(", ")
